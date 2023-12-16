58d39ae52c3931836f9b0284a9d0492a4457d087
frankosgit <132131624+frankosgit@users.noreply.github.com>
2023-12-16 22:54:49 +0100

    git init starting design

--- postcss.config.js
+++ postcss.config.js
@@ -1,6 +1,13 @@
 module.exports = {
+  theme: {
+    extend: {
+      fontSize: {
+        '10xl': '10rem',
+      },
+    },
+  },
   plugins: {
     tailwindcss: {},
     autoprefixer: {},
   },
-}
+};
