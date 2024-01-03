c27b3c274da065a17f6702db83819ff035e10db7
frankosgit <132131624+frankosgit@users.noreply.github.com>
2024-01-03 21:09:27 +0100

    added responsiveness udpated, swapped title for SVG

--- src/app/components/hero/hero.tsx
+++ src/app/components/hero/hero.tsx
@@ -27,13 +27,31 @@ const Hero: React.FC<ContactProps> = ({ onContactClick, showContactForm }) => {
       <h1 className="mb-1 w-full text-left font-mono text-white/30 lg:text-right 2xl:text-lg pt-6 z-0">
         PRESENTED BY <span className="text-white underline">FRANCIS JONES</span>
       </h1>
-      <div className="flex justify-between">
-        <h1 className="text-10xl px-2 pt-0 mt-0">FULLSTACK</h1>
-      </div>
 
-      <div className="mt-14 flex">
-        <div className="ml-6 flex flex-col lg:justify-between">
-          {/* PORTFOLIO Link */}
+    <div className='w-full mt-10'>
+    <style jsx global>
+    {`
+      @font-face {
+        font-family: 'Trap';
+        src: url('../app/assets/fonts/Trap_V1.1/Trap-Black.otf') format('opentype');
+        font-weight: normal;
+      }
+    `}
+  </style>
+  <svg viewBox="0 0 500 100" xmlns="http://www.w3.org/2000/svg" preserveAspectRatio="xMidYMid meet" className="w-full h-auto">
+    <rect width="100%" height="100%" fill="#121417" />
+    <text x="50%" y="50%" alignment-baseline="middle" text-anchor="middle" fill="white"  font-size="85">
+      FULLSTACK
+    </text>
+  </svg>
+</div>
+
+
+
+
+      <div className="mt-14 flex flex-col lg:flex-row pr-6">
+        <div className="flex flex-col lg:justify-between mb-6">
+
           <div
             className={`font-mono ${
               showContactForm ? 'text-white/30' : 'text-white'
@@ -42,7 +60,7 @@ const Hero: React.FC<ContactProps> = ({ onContactClick, showContactForm }) => {
           >
             PORTFOLIO {!showContactForm && <span className="ml-1">▼</span>}
           </div>
-          {/* CONTACT Link */}
+
           <div
             className={`font-mono ${
               showContactForm ? 'text-white' : 'text-white/30'
@@ -63,8 +81,8 @@ const Hero: React.FC<ContactProps> = ({ onContactClick, showContactForm }) => {
 
           </div>
         </div>
-        <div className="max-w-xl leading-8 space-y-2 tracking-wider ml-auto font-mono">
-          Stockholm based. Pivoting from a career in healthcare and entrepenurship
+        <div className="max-w-xl leading-8 space-y-2 tracking-wider ml-auto font-mono md:flex-col md:text-left">
+          Stockholm based. Pivoting from a career in healthcare and entrepreneurship
           to pursue a career in tech and problem-solving. Humble, curious and quick to
           learn. <br /> Looking for opportunities for an internship autumn 2024.
         </div>
--- src/app/components/portfolio/portfolio.tsx
+++ src/app/components/portfolio/portfolio.tsx
@@ -9,7 +9,7 @@ const Portfolio = () => {
         <ul className='w-full'>
             <li className='mb-16 z-10'>
             <a href="" className={`block w-full border-t border-t-neutral-700 pt-4 ${styles['portfolio-item']}`} style={{ filter: 'grayscale(100%)', transition: 'filter 0.3s ease-in-out' }}>
-                    <h1 className="text-4xl uppercase tracking-wide lg:text-7xl 2xl:text-[7.5rem]">
+                    <h1 className="text-4xl uppercase tracking-wide lg:text-portfolio 2xl:text-[7.5rem]">
                      E-CV and Skills
                     </h1>
                     <div className="font-mono uppercase 2xl:text-lg">Tech stack: Next JS, TS, TW, CSS animations & Framer motion</div>
@@ -18,7 +18,7 @@ const Portfolio = () => {
             </li>
             <li className='mb-16'>
                 <a href="" className='block w-full border-t border-t-neutral-700 pt-4 z-10'>
-                <h1 className="text-4xl uppercase tracking-wide lg:text-7xl 2xl:text-[7.5rem]">
+                <h1 className="text-4xl uppercase tracking-wide lg:text-portfolio 2xl:text-[7.5rem]">
                     Fullstack e-commerce store
                     </h1>
                     <div className="mt-3 font-mono uppercase 2xl:text-lg">Tech stack: Next JS, TS, DaisyUI, SQL & Auth with supabase </div>
@@ -27,7 +27,7 @@ const Portfolio = () => {
             </li>
             <li className='mb-16'>
                 <a href="" className='block w-full border-t border-t-neutral-700 pt-4 z-10'>
-                <h1 className="text-4xl uppercase tracking-wide lg:text-7xl 2xl:text-[7.5rem]">
+                <h1 className="text-4xl uppercase tracking-wide lg:text-portfolio 2xl:text-[7.5rem]">
                         Contributions to fullstack parking application
                     </h1>
                     <div className="mt-3 font-mono uppercase 2xl:text-lg">Tech stack: Next JS, TS, MongoDB</div>
