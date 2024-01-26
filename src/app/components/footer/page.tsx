cee69dccc5f9740bafcb0583201e9c61d6a86232
frankosgit <132131624+frankosgit@users.noreply.github.com>
2024-01-26 09:46:34 +0100

    footer update

--- src/app/components/footer/page.tsx
+++ src/app/components/footer/page.tsx
@@ -3,12 +3,10 @@ import React from 'react'
 const Footer = () => {
   return (
     <div>
-    <footer className="flex items-center justify-center w-full h-24 border-t border-neutral-700">
-        <ul>
-            <li className="font-mono text-white/30">Francis Jones</li>
-            <li className="font-mono text-white/30">Stockholm, Sweden</li>
-            <li className="font-mono text-white/30">devFrancisJones@gmail.com</li>
-        </ul>
+    <footer className="flex items-center justify-center w-full h-24 border-t border-neutral-700 text-xs">
+
+            <h6 className="font-mono text-white/30">Website built in React, NextJS (App router & Server actions), Tailwind CSS, Framer Motion, React email & Resend, Vercel hosting</h6>
+
         </footer>
     </div>
   )
